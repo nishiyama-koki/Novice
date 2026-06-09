e468b5fa1fa7d590796cca2f5a86c9bec3565744
nsym.kuk <k025g1028@g.neec.ac.jp>
2026-06-09 11:08:00 +0900

    3次元衝突判定（三角形と線）

--- main.cpp
+++ main.cpp
@@ -30,146 +30,148 @@ void DrawGrid(const Matrix4x4& viewPlojectionMatrix4x4, const Matrix4x4& viewPor
 		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), 0xFFFFFF77);
 	}
 }
-
-Vector3 Perpendicular(const Vector3& v) {
-	if (v.x != 0.0f || v.y != 0.0f) {
-		return {-v.y, v.x, 0.0f};
+struct Triangle {
+	Vector3 vertices[3];
+};
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	Vector3 screenVertices[3];
+	for (uint32_t i = 0; i < 3; ++i) {
+		screenVertices[i] = Transform(Transform(triangle.vertices[i], viewProjectionMatrix), viewportMatrix);
 	}
-	return {0.0f, -v.z, v.y};
+	Novice::DrawLine(int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x), int(screenVertices[1].y), color);
+	Novice::DrawLine(int(screenVertices[1].x), int(screenVertices[1].y), int(screenVertices[2].x), int(screenVertices[2].y), color);
+	Novice::DrawLine(int(screenVertices[2].x), int(screenVertices[2].y), int(screenVertices[0].x), int(screenVertices[0].y), color);
 }
+bool isCollision(const Triangle& triangle, const Segment& segment) {
+	Vector3 v1 = Subtract(triangle.vertices[1], triangle.vertices[0]);
+	Vector3 v2 = Subtract(triangle.vertices[2], triangle.vertices[1]);
+	Vector3 normal = Cross(v1, v2);
+	Vector3 normalNormalize = Normalize(normal);
 
-void DrawPlane(const Plane& plane, const Matrix4x4& viewPlojectionMatrix, const Matrix4x4& viewPortMatrix, uint32_t color) {
-	Vector3 center = Multiply(plane.normal, plane.distance);
-	Vector3 perpendiculars[4];
-	perpendiculars[0] = Normalize(Perpendicular(plane.normal));
-	perpendiculars[1] = {-perpendiculars[0].x, -perpendiculars[0].y, -perpendiculars[0].z};
-	perpendiculars[2] = Cross(plane.normal, perpendiculars[0]);
-	perpendiculars[3] = {-perpendiculars[2].x, -perpendiculars[2].y, -perpendiculars[2].z};
-
-	Vector3 points[4];
-	for (int32_t index = 0; index < 4; ++index) {
-		Vector3 extend = Multiply(perpendiculars[index], 2.0f);
-		Vector3 point = Add(center, extend);
-		points[index] = Transform(Transform(point, viewPlojectionMatrix), viewPortMatrix);
-	}
-	Novice::DrawLine(int(points[0].x), int(points[0].y), int(points[2].x), int(points[2].y), color);
-	Novice::DrawLine(int(points[2].x), int(points[2].y), int(points[1].x), int(points[1].y), color);
-	Novice::DrawLine(int(points[1].x), int(points[1].y), int(points[3].x), int(points[3].y), color);
-	Novice::DrawLine(int(points[3].x), int(points[3].y), int(points[0].x), int(points[0].y), color);
-}
-
-bool IsCollision(const Segment& segment, const Plane& plane) {
-	float dot = Dot(plane.normal, segment.diff);
+	float dot = Dot(normalNormalize, segment.diff);
 	if (dot == 0.0f) {
 		return false;
 	}
 
-	float t = (plane.distance - Dot(plane.normal, segment.origin)) / dot;
-	if (t >= 0 && t <= 1) {
+	float distance = Dot(triangle.vertices[0], normalNormalize);
+	float t = (distance - Dot(segment.origin, normalNormalize)) / dot;
+	if (t < 0.0f || t > 1.0f) {
+		return false;
+	}
+
+	Vector3 p = Add(segment.origin, Multiply(segment.diff, t));
+	Vector3 v01 = Subtract(triangle.vertices[1], triangle.vertices[0]);
+	Vector3 v12 = Subtract(triangle.vertices[2], triangle.vertices[1]);
+	Vector3 v20 = Subtract(triangle.vertices[0], triangle.vertices[2]);
+	Vector3 v0p = Subtract(p, triangle.vertices[0]);
+	Vector3 v1p = Subtract(p, triangle.vertices[1]);
+	Vector3 v2p = Subtract(p, triangle.vertices[2]);
+	Vector3 cross01 = Cross(v01, v0p);
+	Vector3 cross12 = Cross(v12, v1p);
+	Vector3 cross20 = Cross(v20, v2p);
+	if (Dot(cross01, normalNormalize) >= 0.0f && Dot(cross12, normalNormalize) >= 0.0f && Dot(cross20, normalNormalize) >= 0.0f) {
 		return true;
 	} else {
 		return false;
 	}
-}
 
-// Windowsアプリでのエントリーポイント(main関数)
+} 
+//Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+// ライブラリの初期化
+Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+// キー入力結果を受け取る箱
+char keys[256] = {0};
+char preKeys[256] = {0};
 
-	Vector3 cameraTranslate{0.0f, 1.9f, -6.49f};
-	Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
+Vector3 cameraTranslate{0.0f, 1.9f, -6.49f};
+Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
+Triangle triangle{
+    {{-1.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}}
+};
+Segment segment{
+    {0.0f, 1.0f,  0.0f},
+    {0.0f, -2.0f, 0.0f}
+};
 
-	Plane plane{
-	    {0.0f, 1.0f, 0.0f},
-        0.0f
-    };
-	Segment segment{
-	    {0.0f, 1.0f,  0.0f},
-        {0.0f, -2.0f, 0.0f}
-    };
+uint32_t segmentColor = WHITE;
 
-	uint32_t segmentColor = WHITE;
+// ウィンドウの×ボタンが押されるまでループ
+while (Novice::ProcessMessage() == 0) {
+	// フレームの開始
+	Novice::BeginFrame();
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+	// キー入力を受け取る
+	memcpy(preKeys, keys, 256);
+	Novice::GetHitKeyStateAll(keys);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+	///
+	/// ↓更新処理ここから
+	///
 
-		///
-		/// ↓更新処理ここから
-		///
+	ImGui::Begin("Window");
 
-		
-
-
-		ImGui::Begin("Window");
-
-		if (ImGui::CollapsingHeader("Camera")) {
-			ImGui::DragFloat3("Camera Translate", &cameraTranslate.x, 0.01f);
-			ImGui::DragFloat3("Camera Rotate", &cameraRotate.x, 0.01f);
-		}
-
-		if (ImGui::CollapsingHeader("Segment")) {
-			ImGui::DragFloat3("Segment Origin", &segment.origin.x, 0.01f);
-			ImGui::DragFloat3("Segment Diff", &segment.diff.x, 0.01f);
-		}
-
-		ImGui::DragFloat3("Plane.Normal", &plane.normal.x, 0.01f);
-		plane.normal = Normalize(plane.normal);
-
-		ImGui::End();
-
-		if (IsCollision(segment, plane)) {
-			segmentColor = RED;
-		} else {
-			segmentColor = WHITE;
-		}
-
-		Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, cameraTranslate);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
-		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
-		Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 1.0f);
-
-		Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
-		Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), viewProjectionMatrix), viewportMatrix);
-
-		///
-		/// ↑更新処理ここまで
-		///
-
-		///
-		/// ↓描画処理ここから
-		///
-
-		DrawGrid(viewProjectionMatrix, viewportMatrix);
-		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), segmentColor);
-		DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
-
-		///
-		/// ↑描画処理ここまで
-		///
-
-		// フレームの終了
-		Novice::EndFrame();
-
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
+	if (ImGui::CollapsingHeader("Camera")) {
+		ImGui::DragFloat3("Camera Translate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("Camera Rotate", &cameraRotate.x, 0.01f);
 	}
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+	if (ImGui::CollapsingHeader("Segment")) {
+		ImGui::DragFloat3("Segment Origin", &segment.origin.x, 0.01f);
+		ImGui::DragFloat3("Segment Diff", &segment.diff.x, 0.01f);
+	}
+
+	if (ImGui::CollapsingHeader("Triangle")) {
+		ImGui::DragFloat3("Vertex0", &triangle.vertices[0].x, 0.01f);
+		ImGui::DragFloat3("Vertex1", &triangle.vertices[1].x, 0.01f);
+		ImGui::DragFloat3("Vertex2", &triangle.vertices[2].x, 0.01f);
+	}
+
+	ImGui::End();
+
+	if (isCollision(triangle, segment)) {
+		segmentColor = RED;
+	} else {
+		segmentColor = WHITE;
+	}
+
+	Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, cameraTranslate);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
+	Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 1.0f);
+
+	Vector3 start = Transform(Transform(segment.origin, viewProjectionMatrix), viewportMatrix);
+	Vector3 end = Transform(Transform(Add(segment.origin, segment.diff), viewProjectionMatrix), viewportMatrix);
+
+	///
+	/// ↑更新処理ここまで
+	///
+
+	///
+	/// ↓描画処理ここから
+	///
+
+	DrawGrid(viewProjectionMatrix, viewportMatrix);
+	Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), segmentColor);
+	DrawTriangle(triangle, viewProjectionMatrix, viewportMatrix, WHITE);
+
+	///
+	/// ↑描画処理ここまで
+	///
+
+	// フレームの終了
+	Novice::EndFrame();
+
+	// ESCキーが押されたらループを抜ける
+	if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+		break;
+	}
+}
+
+// ライブラリの終了
+Novice::Finalize();
+return 0;
 }
